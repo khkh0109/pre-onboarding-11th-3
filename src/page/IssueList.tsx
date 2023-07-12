87e6c07aecf4eeb2fd9822bc522259931087508c
khkh0109 <ist200120@gmail.com>
2023-07-13 02:01:30 +0900

    design: IssueList 페이지 컴포넌트 추가

--- src/page/IssueList.tsx
+++ src/page/IssueList.tsx
@@ -1,5 +1,15 @@
-function IssueList(): JSX.Element {
-  return <div>IssueList</div>;
+import Issue from "../components/IssueList/Issue";
+import Advertising from "../components/IssueList/Advertising";
+
+function IssueList() {
+  return (
+    <ul>
+      {[1, 2, 3, 4, 5].map(item => {
+        return <Issue key={item} />;
+      })}
+      <Advertising />
+    </ul>
+  );
 }
 
 export default IssueList;
