8a63fe22ce8c9c23eac641623cf824c08ed34055
khkh0109 <ist200120@gmail.com>
2023-07-13 12:41:33 +0900

    feat: App 컴포넌트에 Header, router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import Header from "./components/common/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header organization="organization" repository="repository" />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
